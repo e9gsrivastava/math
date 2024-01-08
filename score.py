f63c12d02ab05ef6b45744981375cc6eb705926a
gaurav srivastava <gaurav.srivastava@enine.school>
2024-01-08 17:39:51 +0530

    update_math

--- score.py
+++ score.py
@@ -4,22 +4,15 @@ import os
 def run_all_math():
     '''this func runs all the functions of math '''
     
-    # all_math_folder=os.listdir()
-    all_math_folder=[]
-
-    for i in range(1, 21):
-        if i<9:
-            all_math_folder.append('math00'+str(i))
-        elif i>9:
-            all_math_folder.append('math0'+str(i))
-    
+    all_folder=os.listdir()
+    all_math_folder=[folder for folder in all_folder  if 'math' in folder]
+    all_math_folder=sorted(all_math_folder)
 
     for folder in all_math_folder:
         ans_path = os.path.join(folder, 'solution.py')
-        print(ans_path)
-        # print(f'\n')
-        # print(f'below is solution for {folder}\n')
-        # os.system( 'python3 '+ ans_path)
+        print(f'\n')
+        print(f'below is solution for {folder}\n')
+        os.system( 'python3 '+ ans_path)
         
 
 
