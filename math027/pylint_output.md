a2d56cee8c56f2270fbe06d0fd1bc6462a341f55
gaurav srivastava <gaurav.srivastava@enine.school>
2024-01-16 17:56:15 +0530

    completed

---
+++ math027/pylint_output.md
@@ -0,0 +1,4 @@
+(venv) fox@enine014:~/math027$ pylint solution.py 
+
+--------------------------------------------------------------------
+Your code has been rated at 10.00/10 (previous run: 10.00/10, +0.00)
---
+++ math027/solution.py
@@ -0,0 +1,32 @@
+"""
+Quadratic Primes
+"""
+
+
+def answer():
+    '''Find the product of the coefficients,a and b, 
+    for the quadratic expression that produces the maximum number of
+      primes for consecutive values of n, starting with n=0.'''
+    def is_prime(n):
+        '''func to find prime num'''
+        if n <= 0:
+            return False
+        for i in range(2, int(n**0.5) + 1):
+            if n % i == 0:
+                return False
+        return True
+
+    hash_dict = {}
+    for a in range(-999, 1000):
+        for b in range(-1000, 1001):
+            num = 0
+
+            while is_prime(num**2 + a * num + b):
+                num += 1
+            hash_dict.update({num: a * b})
+    key = sorted(hash_dict, reverse=True)[0]
+    return hash_dict[key]
+
+
+if __name__ == "__main__":
+    print(answer())
