593b638a3a1788b54d0b03f22d229824bedd7e23
e9gsrivastava <gaurav.srivastava@enine.school>
2024-01-23 11:02:58 +0530

    Completed

---
+++ math030/pylint_output.md
@@ -0,0 +1,4 @@
+(venv) fox@enine014:~/math030$ pylint solution.py 
+
+-------------------------------------------------------------------
+Your code has been rated at 10.00/10 (previous run: 8.57/10, +1.43)
---
+++ math030/solution.py
@@ -0,0 +1,27 @@
+"""
+Digit Fifth Powers
+"""
+
+
+def answer():
+    '''Find the sum of all the numbers that can be written
+    as the sum of fifth powers of their digits.'''
+    return solver(5)
+
+
+def solver(n):
+    '''Can you generalize the solution in Part A to find the sum of all
+    the numbers that can be written as the sum of the nth powers of their digits?'''
+    final_sum =0
+    for i in range(2, 10000000):
+        current_sum = 0
+        for j in range(len(str(i))):
+            current_sum += int(str(i)[j]) ** n
+        if current_sum == i:
+            final_sum+=i
+    return final_sum
+
+
+if __name__ == "__main__":
+    print(answer())
+    print(solver())
